832f84567b251148d6565d2d419d4d9882ae8472
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-07-23 18:11:37 +0530

    Create tom.py

---
+++ tom.py
@@ -0,0 +1,30 @@
+from pyrogram import Client, idle, filters, enums
+from db import is_voted, save_vote
+import time
+from SafoneAPI import SafoneAPI
+import os
+import asyncio
+from html_telegraph_poster.upload_images import upload_image
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InputMediaPhoto
+from jikanpy import Jikan
+import signal
+from io import BytesIO
+import sys
+import random
+import base64
+import aiohttp
+import requests
+from html_telegraph_poster import TelegraphPoster
+api_id = 3845818
+api_hash = "95937bcf6bc0938f263fc7ad96959c6d"
+bot_token = "7005003917:AAG1GwMAm4uFxMOzvesg1vTWRjVX0hHKStM"
+app = Client("anime_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+
+post
+
+@app.on_message(filters.private & filters.command("start"))
+async def handle_message(bot, cmd: Message):
+    user_id = cmd.from_user.id
+    username = cmd.from_user.username
+    uname = f"@{username}"
+  
